06a09f6b971783457112ab0e29a3e4e50c9f3b55
Noah Baumgartner <noah.baumgartner1@students.unibe.ch>
2026-06-18 13:29:31 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "pages#home"
-  get  "/leistungen"     => "pages#leistungen",    as: :leistungen
-  get  "/team"           => "pages#team",          as: :team
-  get  "/kontakt"        => "pages#kontakt",        as: :kontakt
-  post "/kontakt/senden" => "pages#contact_submit", as: :contact_submit
 end
